b123a3a001e132cb207152790fc331770d32cb0c
jimmy <jimmyjimmyhuang1114@gmail.com>
2025-07-13 18:41:29 +0000

    init

--- build/obs_filiter/CMakeFiles/obs_filiter_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/obs_filiter_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/obs_filiter_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
